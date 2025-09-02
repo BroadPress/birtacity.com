05dc80a8602beb2a12cdfa532705a13d95386d5a
Sanjaya Khatiwada <96878472+sanzay34@users.noreply.github.com>
2025-09-02 10:49:51 +0545

    Prachalit section started in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import Navbar from '../src/components/Navbar'
 function App() {
 	return (
 		<div className="">
-			<Navbar/>
 			<div className=" px-6 py-3 md:px-14 md:py-7 ">
+				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
-					
 				</Routes>
 			</div>
-			
 		</div>
 	);
 }
