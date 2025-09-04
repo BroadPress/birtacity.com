51c15b61d51fbf28e494f8fb199d12006adebe8e
Sanjaya Khatiwada <96878472+sanzay34@users.noreply.github.com>
2025-09-04 11:29:24 +0545

    Categories pramuk and prachalit section completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/Categories" element={<Categories />} />
+					<Route path="/categories" element={<Categories />} />
 				</Routes>
 			</div>
 			<Footer/>
